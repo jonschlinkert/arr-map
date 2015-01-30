2c0e7ce42fbdfd9046b70a0f6cd5f93e36b4b608
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-30 18:27:42 -0500

    add tests

---
+++ test.js
@@ -0,0 +1,25 @@
+/*!
+ * arr-map <https://github.com/jonschlinkert/arr-map>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var assert = require('assert');
+var map = require('./');
+
+it('should iterate over the elements in an array:', function () {
+  var actual = map(['a', 'b', 'c'], function (ele) {
+    return ele + ele;
+  });
+  assert.deepEqual(actual, ['aa', 'bb', 'cc']);
+});
+
+it('should expose the index as the second param:', function () {
+  var actual = map(['a', 'b', 'c'], function (ele, i) {
+    return i + ele;
+  });
+  assert.deepEqual(actual, ['0a', '1b', '2c']);
+});
