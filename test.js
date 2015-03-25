9bada714fc3802186bf5c6c05fee16587e731696
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-24 23:28:30 -0400

    lint

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 /*!
  * arr-map <https://github.com/jonschlinkert/arr-map>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
+/* deps:mocha */
 var assert = require('assert');
 var map = require('./');
 
