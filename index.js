e9ff02b447bb869a4d12a110b95a0c30a1e8a187
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-26 19:44:03 -0500

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * arr-map <https://github.com/jonschlinkert/arr-map>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
